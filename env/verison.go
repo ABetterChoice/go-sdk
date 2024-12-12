4de9f2744d99568abf172f23f155425823e00cf9
leoshli <leoshli@tencent.com>
2024-12-12 19:24:49 +0800

    version

--- env/verison.go
+++ env/verison.go
@@ -3,9 +3,9 @@ package env
 
 const (
 	// SDKVersion SDK version
-	SDKVersion = "GO_SDK_v0.1.6"
+	SDKVersion = "GO_SDK_v0.1.7"
 	// SDKType SDK language type
 	SDKType = "GO"
 	// Version version
-	Version = "v0.1.6"
+	Version = "v0.1.7"
 )
